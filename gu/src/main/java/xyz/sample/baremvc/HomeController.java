46e036fdf096b53cb2ca7d107fe7acd5e931f91c
xujinxing <xujinxing@jinx>
2013-07-13 23:54:23 +0800

    第一版

---
+++ gu/src/main/java/xyz/sample/baremvc/HomeController.java
@@ -0,0 +1,34 @@
+package xyz.sample.baremvc;  
+
+   
+
+import org.springframework.stereotype.Controller;  
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+
+   
+
+/**  
+
+ * Handles requests for the application home page.  
+
+ */ 
+
+@Controller 
+@RequestMapping(value = "/")
+public class HomeController {  
+
+   
+
+    @RequestMapping(value = "/")  
+
+    public String home() {  
+
+        System.out.println("HomeController: Passing through...");  
+
+        return "WEB-INF/views/home.jsp";  
+
+    }  
+
+} 
+
